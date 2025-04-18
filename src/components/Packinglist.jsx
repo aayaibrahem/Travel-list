81d120efdab37ab88f710333292e755849f14591
Aya ibrahem <aayaibraheemm@gmail.com>
2025-04-18 22:15:11 +0200

    added

--- src/components/Packinglist.jsx
+++ src/components/Packinglist.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
 import Item from "./Item";
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 1, packed: false },
-];
 
 export default function Packinglist({
   items,
